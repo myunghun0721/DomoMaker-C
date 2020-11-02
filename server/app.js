d9c2cf60cbc1a1ba83d5cee1d7c8759192b8db3d
myunghun0721 <hxc9272@rit.edu>
2020-11-02 18:54:14 -0500

    circle ci fixed

--- server/app.js
+++ server/app.js
@@ -31,7 +31,7 @@ mongoose.connect(dbURL, mongooseOptions, (err) => {
 
 let redisURL = {
   hostname: 'redis-14003.c61.us-east-1-3.ec2.cloud.redislabs.com',
-  port: 14003
+  port: 14003,
 };
 
 let redisPASS = 'QHzceFvdW32UQLQuKQQS8bKhyOrdHjtP';
@@ -49,28 +49,29 @@ const redisClient = redis.createClient({
 // Pull in our routes
 const router = require('./router.js');
 
-
-
 const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.disable('x-powered-by');
 app.use(compression());
+
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
+
 app.use(session({
   key: 'sessionid',
   store: new RedisStore({
-  	client: redisClient
+    client: redisClient,
   }),
   secret: 'Domo Arigato',
   resave: true,
   saveUninitialized: true,
   cookie: {
-  	httpOnly: true,
+    httpOnly: true,
   },
 }));
+
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
@@ -78,10 +79,10 @@ app.use(cookieParser());
 
 // needs to come after cookieParser() and session()
 app.use(csrf());
-app.use((err,req,res,next) => {
-	if (err.code !== 'EBADCSRFTOKEN') return next(err);
-	console.log('Missing CSRF token');
-	return false;
+app.use((err, req, res, next) => {
+  if (err.code !== 'EBADCSRFTOKEN') return next(err);
+  console.log('Missing CSRF token');
+  return false;
 });
 
 
--- server/router.js
+++ server/router.js
@@ -2,7 +2,7 @@ const controllers = require('./controllers');
 const mid = require('./middleware');
 
 const router = (app) => {
-  app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage );
+  app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
   app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
   app.get('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signupPage);
   app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
